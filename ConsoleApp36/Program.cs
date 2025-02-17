102445819a4bb00ce55eecea7ecf00c9ddfed248
Максим <maximmmd13@gmail.com>
2025-02-17 23:06:05 +0200

    Task 2

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    /// <summary>
+    /// 2) Инициализировать одномерный массив с использованием конструктора , без указания размерности массива и без клавиатуры. 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[] { 4, 5, 4, 6, 7 };
+            foreach (int element in omas)
+            {
+                Console.Write($"{element}\t");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
