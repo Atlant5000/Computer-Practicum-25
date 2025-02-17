c1c052b6a24a12aedf0b50fca5b0d6f968259c2d
Максим <maximmmd13@gmail.com>
2025-02-17 23:06:34 +0200

    Task 3

---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{
+    /// <summary>
+    /// 3) Заполнить одномерный массив соответствующими индексами и вывести с использованием цикла foreach.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[7];
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = i;
+            }
+
+            foreach (int el in omas)
+            {
+                Console.Write($"{el}\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
